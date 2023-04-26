72e1cd7b1adba48a3f6f898f9590c3632107cc3a
elvin <elvin@gmail.com>
2023-04-26 10:17:56 +0400

    df

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
   // search-box open close js code
   let navbar = document.querySelector(".navbar");
-  // let searchBox = document.querySelector(".search-box .bx-search");
-  // let searchBoxCancel = document.querySelector(".search-box .bx-x");
-  
-  // searchBox.addEventListener("click", ()=>{
-  //   navbar.classList.toggle("showInput");
-  //   if(navbar.classList.contains("showInput")){
-  //     searchBox.classList.replace("bx-search" ,"bx-x");
-  //   }else {
-  //     searchBox.classList.replace("bx-x" ,"bx-search");
-  //   }
-  // });
+
   
   // sidebar open close js code
   let navLinks = document.querySelector(".nav-links");
@@ -29,7 +19,7 @@
   designerArrow.onclick = function() {
    navLinks.classList.toggle("show1");
   }
-   let moreArrow = document.querySelector(".more-arrow");
+   let moreArrow = document.querySelector(".developer-arrow");
    moreArrow.onclick = function() {
     navLinks.classList.toggle("show2");
    }
